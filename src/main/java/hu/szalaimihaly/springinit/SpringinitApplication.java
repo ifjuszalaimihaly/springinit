ea190675c7dc82c8120402d31d923c032d3aaa26
Mihály Szalai <ifjuszalaimihaly@gmail.com>
2019-02-08 11:39:46 +0100

    Created basic models

--- src/main/java/hu/szalaimihaly/springinit/SpringinitApplication.java
+++ src/main/java/hu/szalaimihaly/springinit/SpringinitApplication.java
@@ -1,10 +1,14 @@
 package hu.szalaimihaly.springinit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringinitApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringinitApplication.class, args);
