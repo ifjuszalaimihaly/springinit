6b63cbd4a4777e37fb3f7acc959a3a61e6892f3b
Mihály Szalai <ifjuszalaimihaly@gmail.com>
2019-02-08 11:54:55 +0100

    Entities refactored project lombok

---
+++ src/main/java/hu/szalaimihaly/springinit/domain/Link.java
@@ -0,0 +1,23 @@
+package hu.szalaimihaly.springinit.domain;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Link {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String url;
+
+    // comments
+}
