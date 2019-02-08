20d592470dead20bf3b0913001e263711232be12
Mihály Szalai <ifjuszalaimihaly@gmail.com>
2019-02-08 09:46:58 +0100

    Adding profiles

--- src/main/java/hu/szalaimihaly/springinit/SpringinitApplication.java
+++ src/main/java/hu/szalaimihaly/springinit/SpringinitApplication.java
@@ -1,7 +1,10 @@
 package hu.szalaimihaly.springinit;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SpringinitApplication {
@@ -9,6 +12,14 @@ public class SpringinitApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringinitApplication.class, args);
         System.out.println("Welcome to Springinit!");
+    }
+
+    @Bean
+    @Profile("dev")
+    CommandLineRunner runner(){
+        return args -> {
+            System.out.println("This is something run in dev mode only");
+        };
     }
 
 }
