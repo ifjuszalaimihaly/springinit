3886f7cd43136ccabc373aaa74595105ea992598
Mihály Szalai <ifjuszalaimihaly@gmail.com>
2019-02-08 14:08:42 +0100

    Added assotiations between links and comments

--- src/main/java/hu/szalaimihaly/springinit/domain/Link.java
+++ src/main/java/hu/szalaimihaly/springinit/domain/Link.java
@@ -8,6 +8,9 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -22,5 +25,6 @@ public class Link {
     @NonNull
     private String url;
 
-    // comments
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 }
